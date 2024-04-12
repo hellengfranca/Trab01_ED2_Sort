2334e4511fe2c94d9a77aa815a8e1697a6d23143
I4ze <Iaze.guilherme25@gmail.com>
2024-04-12 15:36:51 -0300

    Conclusão da 2º questão

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Sort.ModifiedSelectSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] V = {3, 2, 5, 7, 3, 2, 1, 8, 7, 6, 4, 3, 3};
+        ModifiedSelectSort selectSort = new ModifiedSelectSort();
+
+        selectSort.selectionSort(V);
+        for (Integer integer : V) {
+            System.out.println(integer);
+        }
     }
 }
--- src/Sort/ModifiedSelectSort.java
+++ src/Sort/ModifiedSelectSort.java
@@ -1,4 +1,24 @@
 package Sort;
 
 public class ModifiedSelectSort {
+    public <T extends Comparable<T>> void selectionSort(T[] A) {
+        for(int i = 0, k = A.length-1; i < A.length/2; i++, k--) {
+            int min = i;
+            int max = k;
+            for(int j = i + 1, l = k - 1; j < A.length; j++, l--) {
+                if(A[j].compareTo(A[min]) < 0) {
+                    min = j;
+                }
+                if(A[l].compareTo(A[max]) > 0) {
+                    max = l;
+                }
+            }
+            T temp1 = A[min];
+            T temp2 = A[max];
+            A[min] = A[i];
+            A[max] = A[k];
+            A[i] = temp1;
+            A[k] = temp2;
+        }
+    }
 }
